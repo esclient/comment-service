fe8df7788685bf4c5e96fa85b8735fdf5ee3af62
Samuel <samueldanielyan@gmail.com>
2025-07-20 22:57:38 +0200

    Update

--- src/commentservice/service/delete_comment.py
+++ src/commentservice/service/delete_comment.py
@@ -1,4 +1,4 @@
-from commentservice.repository.delete_comments import delete_comments as repo_delete_comments
+from commentservice.repository.delete_comment import delete_comments as repo_delete_comments
 
 def delete_comments(comments_id: int) -> int:
     return repo_delete_comments(comments_id)
