58802e4add28d55e3185c3b0d9ce9a07ccb03beb
timavar101 <gorodilovtima@mail.ru>
2025-08-26 22:19:23 +0600

    форматирование по black

--- src/commentservice/handler/get_comments.py
+++ src/commentservice/handler/get_comments.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+
 import grpc
 from google.protobuf.timestamp_pb2 import Timestamp
 
@@ -32,15 +33,14 @@ def GetComments(
 ) -> comment_pb2.GetCommentsResponse:
     comments = service.get_comments(mod_id=request.mod_id)
     return comment_pb2.GetCommentsResponse(
-        mod_id=request.mod_id, 
-        comments=convertCommentsToProto(comments)
+        mod_id=request.mod_id, comments=convertCommentsToProto(comments)
     )
 
 
 def convertCommentToProto(comment: Comment) -> comment_pb2.Comment:
     # Преобразуем datetime в Timestamp
     created_at_ts = dt_to_ts(comment.created_at)
-    
+
     comment_proto = comment_pb2.Comment(
         id=comment.id,
         author_id=comment.author_id,
@@ -50,10 +50,14 @@ def convertCommentToProto(comment: Comment) -> comment_pb2.Comment:
 
     if comment.edited_at is not None:
         edited_at_ts = dt_to_ts(comment.edited_at)
-        comment_proto.edited_at.CopyFrom(edited_at_ts)  # Правильное копирование Timestamp
+        comment_proto.edited_at.CopyFrom(
+            edited_at_ts
+        )  # Правильное копирование Timestamp
 
     return comment_proto
 
 
-def convertCommentsToProto(comments: list[Comment]) -> list[comment_pb2.Comment]:
+def convertCommentsToProto(
+    comments: list[Comment],
+) -> list[comment_pb2.Comment]:
     return [convertCommentToProto(i) for i in comments]
